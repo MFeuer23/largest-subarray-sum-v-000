cbba2665b1bb1d1f884c4ec7585af876afe1d365
Mallory Feuer <mallory.feuer@gmail.com>
2019-04-08 19:54:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function largestSubarraySum (arr) {
+  let sum = 0;
   let subArr = arr.slice(0, arr.indexOf(Math.min(...arr)))
-  console.log(subArr)
+  for (let n of subArr) {
+    sum += n
+  }
+  return sum;
 }
